6491defcf7a85998ffe21ada6d3e9b8ce9631826
Fahim Ahmed <fahimahmed8798740@gmail.com>
2022-02-04 00:41:30 +0600

    second push(text)

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,22 @@ void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Text("Bmi Calculator"),
+      home: BmiCalculation(),
     ),
   );
 }
+
+class BmiCalculation extends StatelessWidget {
+  const BmiCalculation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Scaffold(
+        body:Text("For Another push"),
+      ),
+    );
+  }
+}
+
 
