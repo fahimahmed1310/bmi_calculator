ecf6e1989d8623b59875118ba38568d07e4d49c3
Fahim Ahmed <fahimahmed8798740@gmail.com>
2022-02-04 00:39:05 +0600

    first text messaege

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Text("Bmi Calculator"),
+    ),
+  );
+}
+
